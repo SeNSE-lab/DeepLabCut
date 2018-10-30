0736bb5b37e12609c386ff4f43c47a5f76dc14e5
nbush257 <nbush257@gmail.com>
2018-10-30 15:50:52 -0500

    Fixed indexing bug in randomFromSeq.py

--- Generating_a_Training_Set/randomFromtSeq.py
+++ Generating_a_Training_Set/randomFromtSeq.py
@@ -19,23 +19,18 @@ def extract_ims(seq,n_ims,p_save=None):
     if p_save is None:
         p_save=p_load
     print('Saving to {}'.format(p_save))    
-    V = pims.NorpixSeq(seq)
+    V = pims.open(seq)
     nframes = V.header_dict['allocated_frames']
-    print('Found {} frames'.format(nframes))
+    print('Found {} frames\n'.format(nframes))
     num_pad = int(np.floor(np.log10(nframes)+1))
-    frame_numbers = np.random.choice(np.arange(nframes),n_ims,replace=False)
+    frame_numbers =np.random.choice(np.arange(nframes),n_ims,replace=False).astype('int')
     frame_numbers.sort()
     for frame_number in frame_numbers:
-        print(frame_number)
-        try:
-            I = V.get_frame(frame_number)
-            io.imsave(os.path.join(p_save,'{}_img{:0{num_pad}d}.png'.format(os.path.splitext(basename)[0],
-                                                                            frame_number,
-                                                                            num_pad=num_pad)),
-                      I)
-        except:
-            print('failed at {}'.format(frame_number))
-            continue
+        I = V.get_frame(int(frame_number))
+        io.imsave(os.path.join(p_save,'{}_img{:0{num_pad}d}.png'.format(os.path.splitext(basename)[0],
+                                                                        frame_number,
+                                                                        num_pad=num_pad)),
+                  I)
     return(0) 
 
 if __name__=='__main__':
@@ -51,10 +46,11 @@ if __name__=='__main__':
 
     n_ims = int(sys.argv[2])
     if os.path.isdir(sys.argv[1]):
-        print('Extracting {} images from {} videos in {}'.format(n_ims,len(sys.argv[1]),sys.argv[1]))
-        for seq in glob.glob(os.path.join(sys.argv[1],'*.seq')):
+        seq_list = glob.glob(os.path.join(sys.argv[1],'*.seq'))
+        print('Extracting {} images from {} videos in {}\n'.format(n_ims,len(seq_list),sys.argv[1]))
+        for seq in seq_list:
             print('working on {}'.format(seq))
             extract_ims(seq,n_ims,p_save)
     else:
-        print('Extracting {} images from {}'.format(n_ims,sys.argv[1]))            
+        print('Extracting {} images from {}\n'.format(n_ims,sys.argv[1]))            
         extract_ims(sys.argv[1],n_ims,p_save)
